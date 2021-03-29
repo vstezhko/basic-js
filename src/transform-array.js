9267a25d65a27f9ed7bd57a96b0cc91a8ea9a3ae
VStezhko <71404755+vstezhko@users.noreply.github.com>
2021-03-29 17:23:11 +0300

    Fix: delete solution for transform-array

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,27 +1,5 @@
 const CustomError = require("../extensions/custom-error");
 
 module.exports = function transform( arr ) {
-  let result_arr = [];
-  if (arr.length === 0) {
-    return result_arr;
-  }
-  for (let i = 0; i < arr.length; i++) {
-    switch (arr[i]) {
-      case '--discard-next':
-        i++;
-        break;
-      case '--discard-prev':
-        result_arr.pop();
-        break;
-      case '--double-next':
-        result_arr.push(arr[i++]);
-        break;
-      case '--double-prev':
-        result_arr.push(arr[i--]);
-        break;
-      default:
-        result_arr.push(arr[i]);
-    }
-  }
-  return result_arr;
+
 };
