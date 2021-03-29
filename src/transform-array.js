7a2acd5ac35a168d69e7922c293a68fa1b60b9a2
vstezhko <viktoriastezhko@gmail.com>
2021-03-29 22:59:27 +0300

    fix: solution transform-array

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,9 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform( arr ) {
+
+
+module.exports =function transform(arr) {
+
   let result_arr = [];
   if (arr.length === 0) {
     return result_arr;
@@ -9,19 +12,49 @@ module.exports = function transform( arr ) {
     switch (arr[i]) {
       case '--discard-next':
         i++;
+
         break;
       case '--discard-prev':
-        result_arr.pop();
-        break;
+        if( i === 0){
+
+          break
+        }  else {
+          result_arr.pop();
+          break;
+        }
+
       case '--double-next':
-        result_arr.push(arr[i++]);
-        break;
+        if(i===(arr.length-1) || !Number.isInteger(arr[i+1])  ){
+          break;
+        } else{
+          result_arr.push(arr[(i+1)]);
+          break;
+        }
+
       case '--double-prev':
-        result_arr.push(arr[i--]);
-        break;
+        if( i === 0){
+          break
+        }  else {
+          if (result_arr.indexOf(arr[i-1]) >= 0){
+            result_arr.push(arr[i-1]);
+          }
+        break;}
       default:
         result_arr.push(arr[i]);
     }
   }
   return result_arr;
+
 };
+
+
+// let a = [1, 2, 3, '--discard-next', 1337, '--double-prev', 4, 5];
+// // let b = ['--double-prev', 1, 2, 3];
+// // let c = [1, 2, 3, '--double-next'];
+// // let d = [1, 2, 3, '--discard-next'];
+// //
+// //
+// console.log(transform(a));
+// // console.log(transform(b));
+// // console.log(transform(c));
+// // console.log(transform(d));
--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,14 +1,60 @@
-const CustomError = require("../extensions/custom-error");
+// const CustomError = require("../extensions/custom-error");
+//
+// class VigenereCipheringMachine {
+//   constructor(type=true) {
+//     this.type = type;
+//     this.letters = ["A", "B", 'C', 'D', 'E', 'F',	'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+//   }
+//
+//
+//
+//   encrypt(message, key) {
+//     // throw new CustomError('Not implemented');
+//     // remove line with error and write your code here
+//     let message_arr = message.split("");
+//     let key_arr = key.split("");
+//     let message_arr_code = message_arr.map(function(elem){this.letters.indexOf(elem)});
+//     let key_arr_code = key_arr.map(function(elem){this.letters.indexOf(elem)});
+//
+//   }
+//   decrypt(message, key) {
+//     // throw new CustomError('Not implemented');
+//     // remove line with error and write your code here
+//   }
+// }
+//
+// module.exports = VigenereCipheringMachine;
 
-class VigenereCipheringMachine {
-  encrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  }    
-  decrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  }
+const letters = ["A", "B", 'C', 'D', 'E', 'F',	'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+
+function encrypt(message, key) {
+  console.log(key.length)
+  // throw new CustomError('Not implemented');
+  // remove line with error and write your code here
+  let message_arr = message.split("");
+  let key_arr = (key.repeat(Math.floor((message_arr.length)/key.length))).split("");
+  let message_arr_code = message_arr.map(function(elem){
+    return letters.indexOf(elem.toUpperCase());
+  });
+  let key_arr_code =key_arr.map(function(elem){
+    return letters.indexOf(elem.toUpperCase());
+  });
+    for (let i = 0; i<=(message_arr_code.length-key_arr_code.length); i++) {
+     key_arr_code.push(letters.indexOf((String(key_arr[i]).toUpperCase())));
+    }
+
+    let encrypt_code = [];
+    for (let i=0; i<message_arr_code.length; i++){
+      if((message_arr_code[i]+key_arr_code[i]) >= 26){    ///больше или равно 26??
+        encrypt_code.push(letters[(message_arr_code[i]+key_arr_code[i]-26)])
+      } else{
+        encrypt_code.push(letters[(message_arr_code[i]+key_arr_code[i])])
+      }
+    }
+    console.log(encrypt_code);
+
+return {message_arr_code, key_arr_code}
 }
 
-module.exports = VigenereCipheringMachine;
+
+console.log(encrypt('attack at dawn!', 'alphonse'));
